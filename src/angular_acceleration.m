b4439d4303536adec41f48b517c97b6751687bda
Junshen Xu <junshenxu1996@gmail.com>
2023-05-03 14:17:22 -0400

    add ang vel and acc

--- src/angular_acceleration.m
+++ src/angular_acceleration.m
@@ -1,7 +1,7 @@
-function [m, s] = angular_velocity(data)
-%angular_velocity  Mean and std of velocity of keypoints.
-%   [M, S] = angulari_velocity(DATA) computes the mean M and standard 
-%   deviation S of angular velocity (in deg/s). 
+function [m, s] = angular_acceleration(data)
+%angular_acceleration  Mean and std of angular acceleration of keypoints.
+%   [M, S] = angulari_acceleration(DATA) computes the mean M and standard 
+%   deviation S of angular acceleration (in deg/s^2). 
 %
 %   The list of angles is as follows.
 %
@@ -17,8 +17,7 @@ radius = streval(joint_coord, '{wrist_l,wrist_r}-{elbow_l,elbow_r}');
 cos_knee = sum(femur .* tibia, 2) ./ (vecnorm(femur, 2, 2) + 1e-10) ./ (vecnorm(tibia, 2, 2) + 1e-10);
 cos_elbow = sum(humerus .* radius, 2) ./ (vecnorm(humerus, 2, 2) + 1e-10) ./ (vecnorm(radius, 2, 2) + 1e-10);
 ang = acos(cat(3, cos_knee, cos_elbow)) / pi * 180;
+ang_acc = abs(diff(ang,2,1)) / dt / dt;
 
-ang_vec = abs(ang(2:end, :, :) - ang(1:end-1, :, :)) / dt;
-
-m = mean(ang_vec, 1);
-s = std(ang_vec, 1);
+m = mean(ang_acc, 1);
+s = std(ang_acc, 1);
